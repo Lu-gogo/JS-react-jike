b7f358ad151c5dec4a22fb692be44912b972fea5
Lu-gogo <XI-LANK@outlook.com>
2025-08-17 10:17:52 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       this is App.js
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import { Button, Space, DatePicker, version } from 'antd';
+
+const App = () => (
+  <div style={{ padding: '0 24px' }}>
+    <h1>antd version: {version}</h1>
+    <Space>
+      <DatePicker />
+      <Button type="primary">Primary Button</Button>
+    </Space>
+
+    <p>App</p>
+  </div>
+);
+
+<p>App</p>
 
 export default App;
