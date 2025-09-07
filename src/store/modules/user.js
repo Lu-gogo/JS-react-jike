09ef8de26de33cf406df3122b7f44ccf1e700d19
Lu-gogo <XI-LANK@outlook.com>
2025-09-07 21:59:15 +0800

    token持久化

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,12 +5,14 @@ const userStore = createSlice({
   name: "user",
   //数据状态
   initialState: {
-    token: ''
+    token: localStorage.getItem('token_key') || ''
   },
   //同步修改方法
   reducers: {
     setToken(state, action) {
       state.token = action.payload
+      //localStorage存储token
+      localStorage.setItem('token_key', action.payload)
     }
   }
 })
