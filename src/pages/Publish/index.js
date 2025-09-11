1dc5c2352bde9e8146b86e092ce8583fadb3cdf5
Lu-gogo <XI-LANK@outlook.com>
2025-09-11 17:06:14 +0800

    图片封面类型切换

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -55,10 +55,16 @@ const Publish = () => {
     createArticleAPI(reqData)
   }
   //上传回调
-  const [imgeList, setimageList] = useState([])
+  const [imageList, setimageList] = useState([])
   const onChange = (value) => {
     // console.log('图片上传', value)
     setimageList(value.fileList)
+  }
+  //选框切换
+  const [imageType, setImageType] = useState(0)
+  const onTypeChange = (value) => {
+    // console.log('切换了', value.target.value)
+    setImageType(value.target.value)
   }
   return (
     <div className="publish">
@@ -74,7 +80,7 @@ const Publish = () => {
         <Form
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ type: 1 }}
+          initialValues={{ type: 0 }}
           onFinish={onFinish}
         >
           <Form.Item
@@ -96,7 +102,7 @@ const Publish = () => {
           </Form.Item>
           <Form.Item label="封面">
             <Form.Item name="type">
-              <Radio.Group>
+              <Radio.Group onChange={onTypeChange}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
                 <Radio value={0}>无图</Radio>
@@ -106,17 +112,19 @@ const Publish = () => {
             listType:决定选择文件筐的外观样式
             showUploadList
              */}
-            <Upload
-              listType="picture-card"
-              showUploadList
-              action={'http://geek.itheima.net/v1_0/upload'}
-              name='image'
-              onChange={onChange}
-            >
-              <div style={{ marginTop: 8 }}>
-                <PlusOutlined />
-              </div>
-            </Upload>
+            {imageType > 0 &&
+              <Upload
+                listType="picture-card"
+                showUploadList
+                action={'http://geek.itheima.net/v1_0/upload'}
+                name='image'
+                onChange={onChange}
+              >
+                <div style={{ marginTop: 8 }}>
+                  <PlusOutlined />
+                </div>
+              </Upload>
+            }
           </Form.Item>
           <Form.Item
             label="内容"
